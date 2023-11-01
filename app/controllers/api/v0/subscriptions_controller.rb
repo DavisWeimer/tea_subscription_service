ee1d1010a45f0e8d227e8d1d28acf27c79149f17
Davis Weimer <davisweimer@gmail.com>
2023-11-01 07:36:40 -0600

    See All Customer Subscriptions — Complete

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V0
     class SubscriptionsController < ApplicationController
+      rescue_from StandardError, with: :handle_error
+
       def create
         subscription = Subscription.new(subscription_params)
 
@@ -14,19 +16,25 @@ module Api
       end
 
       def destroy
-        begin
-          subscription = Subscription.find(params[:subscription_id])
-          subscription.update(status: 'cancelled')
-          render json: { message: "Subscription successfully cancelled" }, status: :ok
-        rescue StandardError => e
-          render json: { errors: [{ detail: e.message }] }, status: :not_found
-        end
+        subscription = Subscription.find(params[:subscription_id])
+        subscription.update(status: 'cancelled')
+        render json: { message: "Subscription successfully cancelled" }, status: :ok
+      end
+
+      def index
+        customer = Customer.find(params[:customer_id])
+        subscriptions = customer.subscriptions
+        render json: SubscriptionSerializer.new(subscriptions), status: :ok
       end
 
       private
 
       def subscription_params
         params.require(:subscription).permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
+      end
+
+      def handle_error(exception)
+        render json: { errors: [{ detail: exception.message }] }, status: :not_found
       end
     end
   end
